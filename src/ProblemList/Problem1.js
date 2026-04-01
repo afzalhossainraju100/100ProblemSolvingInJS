a5bcf13f4e48c5843fff26bc002e4c91ee6b8c21
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-04-02 00:04:57 +0600

    resolve problem 1

--- src/ProblemList/Problem1.js
+++ src/ProblemList/Problem1.js
@@ -1,18 +1,17 @@
 //Problem 1 — Two Sum
 //Return indices of two numbers that add up to target.
-function twoSum(nums, target){
+function twoSum(nums,target){
     const map = new Map();
 
-    for(let i=0; i < nums.length; i++){
-        const needed = target - nums[i];
-        
+    for(let i = 0; i<nums.length; i++){
+        const needed = target - nums[i]; 
+
         if(map.has(needed)){
             return [map.get(needed), i];
-
         }
         map.set(nums[i], i);
     }
     return [];
 }
 
-console.log(twoSum([2,11,7,15],9));
+console.log(twoSum([2,7,11,15], 9));
